0357ca43e36421ffbe9e5003fc2bef72c9872383
Riccardo Fusari <ric.fusari@gmail.com>
2023-12-07 19:07:07 +0100

    Automated sync:  M booth_mul/test.sv

--- booth_mul/test.sv
+++ booth_mul/test.sv
@@ -1,10 +1,7 @@
-`timescale 1ns / 1ps
-
 module daddaTree (
   input logic [5:0][12:0] ops,
   output logic [19:0] result
 );
-
 
 logic [5:0][17:0] M0;
 logic [5:0][17:0] M1;
@@ -29,9 +26,6 @@ assign M1[0][12:0] = M0[0][12:0];
 assign M1[1][14:0] = M0[1][14:0];
 assign M1[2][16:0] = M0[2][16:0];
 assign M1[3][13:0] = M0[3][12:0];
-
-
-
 assign M1[0][17:13] = M0[4][17:13];
 assign M1[1][17:14] = M0[3][17:14];
 
@@ -46,15 +40,13 @@ generate
       halfAdder HA2 (M1[3][i], M1[4][i], M2[2][i], M2[3][i+1]);
   end 
 endgenerate
-  
+
 
 //l = 6 -> non regular adders
 
 halfAdder HA0 (M1[0][6], M1[1][6], M2[0][6], M2[1][7]);
 halfAdder HA1 (M1[0][7], M1[1][7], M2[0][7], M2[1][8]);
 fullAdder FA0 (M1[0][13], M1[1][13], M1[2][13], M2[0][13], M2[3][14]);
-
-
 
 //l = 6 -> forming the next level matrix
 assign M2[0][17:14] = M1[0][17:14]; //LEFT
@@ -75,7 +67,6 @@ assign M2[2][13] = M1[3][13];
 
 
 // l = 4 -> set of central adders
-
 generate 
   for (i = 6; i<= 14; i = i+1) begin: M2ADD
     fullAdder FA2 (M2[0][i], M2[1][i], M2[2][i], M3[0][i], M3[1][i+1]);
@@ -83,12 +74,9 @@ generate
 endgenerate
 
 // l = 4 ->  non regular adders
-
 halfAdder HA3 (M2[0][4], M2[1][4], M3[0][4], M3[1][5]);
 halfAdder HA4 (M2[0][5], M2[1][5], M3[0][5], M3[1][6]);
 halfAdder HA5 (M2[0][15], M2[1][15], M3[0][15], M3[1][16]);
-  
-  
 // l = 4 -> forming the next level matrix
 
 assign M3[0][3:0]   = M2[0][3:0];
@@ -102,15 +90,15 @@ assign M3[2][17:16] = M2[2][17:16];
 assign M3[2][14:6]  = M2[3][14:6];
 
 
-  
+
 // l = 3 -> set of central adders
-  
+
 generate
   for (i=4; i<= 15; i = i+1) begin: M3ADD
     fullAdder FA3 (M3[0][i], M3[1][i], M3[2][i], fastAdd[0][i], fastAdd[1][i+1]);
 end
 endgenerate
-  
+
 // l = 3 -> non regular adders
 
 halfAdder HA6 (M3[0][2], M3[1][2], fastAdd[0][2], fastAdd[1][3]);
@@ -126,11 +114,7 @@ assign fastAdd[1][2]   = M3[2][2];
 assign fastAdd[1][1]   = 0;
 assign fastAdd[0][18]  = 0;
 
-
-
 // final sum (2 operands) adder instantiation
-
-
 CSA CSA0 (fastAdd[0][18:0], fastAdd[1][18:0], 0, result[18:0], result[19]);
 
 endmodule
