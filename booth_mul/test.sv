e1890e5212b790725c5dcf59978d44d958b3a003
manucorona <s305612@studenti.polito.it>
2023-12-07 09:59:00 -0800

    reversed

--- booth_mul/test.sv
+++ booth_mul/test.sv
@@ -28,31 +28,36 @@ endmodule
 
 
 
-  logic [0:5][0:17] M1;
-  logic [0:4][0:17] M2;
-  logic [0:3][0:17] M3;
-  logic [0:1][0:18] fastAdd;
+  logic [5:0][17:0] M0;
+  logic [5:0][17:0] M1;
+  logic [4:0][17:0] M2;
+  logic [3:0][17:0] M3;
+  logic [1:0][18:0] fastAdd;
 
   genvar i;
 
  generate 
     for (i = 0; i<5; i= i+1) begin 
  
- assign M1[i][0 + 2*i : 12 + 2*i]  = ops[i];
- assign M1[i+1][2*i] = 1;
+ assign M0[i][12 + 2*i : 0 + 2*i]  = ops[i];
+ assign M0[i+1][2*i] = 1;
 end
 endgenerate
 
 
   // adjust the elements positions (form a V-shape)
 
- assign M1[0][13:17] = M1[4][13:17];
- assign M1[1][14:17] = M1[3][14:17];
+ assign M1[0][12:0] = M0[0][0:12];
+ assign M1[1][14:0] = M0[1][0:14];
+ assign M1[2][16:0] = M0[2][0:16];
+ assign M1[3][13:0] = M0[3][0:12];
+
+
+
+ assign M1[0][13:17] = M0[4][13:17];
+ assign M1[1][14:17] = M0[3][14:17];
 
  
-
-
-
 // START COMPRESSING THE MATRIX 
 
 // l= 6 -> set of central adders 
@@ -93,20 +98,6 @@ endgenerate
 
 
 
-    
-  
-// l = 4 -> forming the next level matrix
-
-  assign M3[0][0:3] = M2[0][0:3];
-  assign M3[1][0:3] = M2[1][0:3];
-  assign M3[2][0:3] = M2[2][0:3];
-  assign M3[1][4:5] = M2[2][4:5];
-  assign M3[2][4:5] = M2[3][4:5];
-  assign M3[0][16:17] = M2[0][16:17];
-  assign M3[1][16:17] = M2[1][16:17];
-  assign M3[2][16:17] = M2[2][16:17];
-  assign M3[2][6:14] = M2[3][6:14];
-
 
 // l = 4 -> set of central adders
 
@@ -124,6 +115,20 @@ endgenerate
  halfAdder HA3 (M2[0][4], M2[1][4], M3[0][4], M3[1][5]);
  halfAdder HA4 (M2[0][5], M2[1][5], M3[0][5], M3[1][6]);
  halfAdder HA5 (M2[0][15], M2[1][15], M3[0][15], M3[1][16]);
+    
+  
+// l = 4 -> forming the next level matrix
+
+  assign M3[0][0:3] = M2[0][0:3];
+  assign M3[1][0:3] = M2[1][0:3];
+  assign M3[2][0:3] = M2[2][0:3];
+  assign M3[1][4:5] = M2[2][4:5];
+  assign M3[2][4:5] = M2[3][4:5];
+  assign M3[0][16:17] = M2[0][16:17];
+  assign M3[1][16:17] = M2[1][16:17];
+  assign M3[2][16:17] = M2[2][16:17];
+  assign M3[2][6:14] = M2[3][6:14];
+
 
   
 // l = 3 -> set of central adders
@@ -139,11 +144,22 @@ endgenerate
 // l = 3 -> non regular adders
 
   halfAdder HA6 (M3[0][2], M3[1][2], fastAdd[0][2], fastAdd[1][3]);
-  halfAdder HA7 (M3[0][3], M3[1][3], fastAdd[0][3], fastAdd[1][3]);
+  halfAdder HA7 (M3[0][3], M3[1][3], fastAdd[0][3], fastAdd[1][4]);
   halfAdder HA8 (M3[0][16],M3[1][16], fastAdd[0][16], fastAdd[1][17]);
   halfAdder HA9 (M3[0][17], M3[1][17], fastAdd[0][17], fastAdd[1][18]);
 
+ // forming fastAdd
+
+  assign fastAdd[0][0:1] = M3[0][0:1];
+  assign fastAdd[1][0] = M3[1][0];
+  assign fastAdd[1][2] = M3[2][2];
+  assign fastAdd[1][1] = 0;
+  assign fastAdd[0][18] = 0;
+
+
+
 // final sum (2 operands) adder instantiation
+
 
  CSA CSA0 (fastAdd[0][0:18], fastAdd[1][0:18], 0, result[0:18], result[19]);
 
