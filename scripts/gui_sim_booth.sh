389dc837493514dee478509f2193aeef62951546
Gianluca Corso <s315100@studenti.polito.it>
2023-12-19 20:53:58 +0100

    end

--- scripts/gui_sim_booth.sh
+++ scripts/gui_sim_booth.sh
@@ -1,40 +1,22 @@
 #!/bin/sh
 
 # Exit immediately if any command fails
-set -e
+#set -e
 
 
 #COMPILE
-
-
-
-vlog ../booth_mul/halfAdder.sv
-vlog ../booth_mul/fullAdder.sv
-vlog ../booth_mul/RCA.sv
-vlog ../booth_mul/CS_block.sv
-vlog ../booth_mul/CSA.sv
-vlog ../booth_mul/LUT.sv
-vlog ../booth_mul/daddatree.sv
-vlog ../booth_mul/multiplier.sv
-
-vlog ../cvfpu_lite/src/cf_math_pkg.sv
-vlog ../cvfpu_lite/src/lzc.sv
-vlog ../cvfpu_lite/src/rr_arb_tree.sv
-vlog ../cvfpu_lite/src/fpnew_pkg.sv
-vlog ../cvfpu_lite/src/fpnew_classifier.sv
-vlog ../cvfpu_lite/src/fpnew_rounding.sv
-vlog ../cvfpu_lite/src/fpnew_fma.sv
-vlog ../cvfpu_lite/src/fpnew_opgroup_fmt_slice.sv
-vlog ../cvfpu_lite/src/fpnew_opgroup_block.sv
-vlog ../cvfpu_lite/src/fpnew_top.sv
-
+vlog -reportprogress 300 -work work ../booth_mul/halfAdder.sv
+vlog -reportprogress 300 -work work ../booth_mul/fullAdder.sv
+vlog -reportprogress 300 -work work ../booth_mul/RCA.sv
+vlog -reportprogress 300 -work work ../booth_mul/CS_block.sv
+vlog -reportprogress 300 -work work ../booth_mul/CSA.sv
+vlog -reportprogress 300 -work work ../booth_mul/LUT.sv
+vlog -reportprogress 300 -work work ../booth_mul/daddatree.sv
+vlog -reportprogress 300 -work work ../booth_mul/multiplier.sv
 #Testbench
-vcom ../cvfpu_lite/tb/clk_gen.vhd
-vcom ../cvfpu_lite/tb/data_gen16.vhd
-vlog ../cvfpu_lite/tb/tb_fpnew_top_rtl.sv
+vlog -reportprogress 300 -work work ./tb_mul.sv
 
-
-vsim -do ./gui_run.do
+vsim -do ./run_booth.do
 
 # Remove work directory
 rm -rf work
